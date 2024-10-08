8d48e50d1c83dcf5b32bb2df8e67c92bed61a8ea
minhaudev <hau95024@donga.edu.vn>
2024-10-08 09:41:05 +0700

    create input I have icon of fontawesome

--- src/components/atoms/Input/index.stories.ts
+++ src/components/atoms/Input/index.stories.ts
@@ -1,32 +1,131 @@
-import { type Meta, type StoryObj } from '@storybook/react';
+import { faMagnifyingGlass, faPencil } from '@fortawesome/free-solid-svg-icons';
+import { Meta, StoryObj } from '@storybook/react';
 
 import Input from './index';
 
 const meta: Meta<typeof Input> = {
   component: Input,
   argTypes: {
+    tags: { control: 'object' },
+    setTags: { action: 'setTags' },
+    setValue: { action: 'setValue' },
+    suffix: { control: 'object' },
+    prefix: { control: 'object' },
+    isDeleteContent: { control: 'boolean' },
     type: { control: 'select', options: ['text', 'number', 'password'] },
     value: { control: 'text' },
-    // Các props khác nếu cần
-    // error: { control: 'boolean' },
-    // helperText: { control: 'text' },
-    // isDisabled: { control: 'boolean' },
-    // isMultiline: { control: 'boolean' },
-    // rows: { control: 'number' },
-    // maxRows: { control: 'number' },
-    // label: { control: 'text' },
-    // placeholder: { control: 'text' },
-    // asterisk: { control: 'boolean' }
+    isError: { control: 'boolean' },
+    helperText: { control: 'text' },
+    isDisabled: { control: 'boolean' },
+    rows: { control: 'number' },
+    maxRows: { control: 'number' },
+    label: { control: 'text' },
+    placeholder: { control: 'text' },
+    isRequired: { control: 'boolean' },
+    handleOnchange: { action: 'handleOnchange' },
   },
 };
 
 export default meta;
+
 type Story = StoryObj<typeof Input>;
 
 // Primary Story
 export const Primary: Story = {
   args: {
+    label: 'Tên Khách hàng',
+    isRequired: true,
     type: 'text',
     value: '',
+    handleOnchange: (e) => console.log(e.target.value),
+  },
+};
+
+export const DisabledInput: Story = {
+  args: {
+    type: 'text',
+    value: 'Chi nhánh mặc định',
+    isDisabled: true,
+  },
+};
+
+export const textRight: Story = {
+  args: {
+    label: 'Công nợ',
+    type: 'text',
+    positionValue: 'right',
+    placeholder: 'nhập công nợ khách hàng',
+    handleOnchange: (e) => console.log(e.target.value),
+  },
+};
+
+export const searchInput: Story = {
+  args: {
+    isDeleteContent: true,
+    prefix: faMagnifyingGlass,
+    label: 'Công nợ',
+    type: 'text',
+    value: 'dữ liệu nhập vào',
+    placeholder: 'Tìm kiếm theo mã đơn hàng,tên, SĐT khách hàng',
+    handleOnchange: (e) => console.log(e.target.value),
+  },
+};
+
+export const numberValue: Story = {
+  args: {
+    isDeleteContent: true,
+    type: 'text',
+    variantsizes: 'large',
+    positionValue: 'right',
+    placeholder: '0',
+    handleOnchange: (e) => console.log(e.target.value),
+  },
+};
+
+export const note: Story = {
+  args: {
+    isNote: true,
+    isDeleteContent: true,
+    type: 'text',
+    label: 'Ghi chú',
+    variantsizes: 'xlarge',
+    placeholder: '0',
+    handleOnchange: (e) => console.log(e.target.value),
+  },
+};
+
+export const tagInput: Story = {
+  args: {
+    tags: ['tag1', 'tag2'],
+    setTags: (newTags) => console.log(newTags),
+    isDeleteContent: true,
+    type: 'text',
+    label: 'Ghi chú',
+    value: '',
+    variantsizes: 'medium',
+    placeholder: 'Gõ kí tự và nhấn enter để thêm thuộc tính',
+    handleOnchange: (e) => console.log(e.target.value),
+  },
+};
+
+export const pencil: Story = {
+  args: {
+    prefix: faPencil,
+    isDeleteContent: true,
+    value: '',
+    variantsizes: 'medium',
+    placeholder: 'Nhập ghi chú đơn hàng',
+    handleOnchange: (e) => console.log(e.target.value),
+  },
+};
+
+export const errorInput: Story = {
+  args: {
+    isError: true,
+    helperText: 'Lỗi rồi nè!',
+    value: '',
+    variantsizes: 'medium',
+    placeholder: 'Nhập ghi chú đơn hàng',
+    handleOnchange: (e) => console.log(e.target.value),
   },
 };
--- src/components/atoms/Input/index.tsx
+++ src/components/atoms/Input/index.tsx
@@ -1,14 +1,144 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { faCircleXmark, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { HStack } from '../HStack';
 
 interface InputProps {
-  value: string;
+  handleOnchange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  className?: string;
+  positionValue?: 'left' | 'right' | 'center';
+  value: string | number;
   type?: string;
+  helperText?: string;
+  isRequired?: boolean;
+  isError?: boolean;
+  rows?: number;
+  maxRows?: number;
+  label?: string;
+  placeholder?: string;
+  isDisabled?: boolean;
+  suffix?: IconProp;
+  prefix?: IconProp;
+  isDeleteContent?: boolean;
+  isInputTag?: boolean;
+  tags?: string[] | undefined;
+  isNote?: boolean;
+  variantsizes?: 'small' | 'medium' | 'large' | 'xlarge' | '2xlarge' | '3xlarge';
+  setTags?: (tags: string[]) => void | [];
+  setValue?: (value: string) => void;
 }
+const positionValue = {
+  left: 'text-left',
+  right: 'text-right',
+  center: 'text-center',
+};
+const variantsizes = {
+  small: ' py-1',
+  medium: ' py-2',
+  large: ' py-3',
+  xlarge: 'h-[120px]',
+  '2xlarge': 'h-[170px]',
+  '3xlarge': 'h-[200px]',
+};
 
-const Input: React.FC<InputProps> = ({ value, type = 'text' }) => {
+const Input: React.FC<InputProps> = (props) => {
+  const [isFocused, setIsFocused] = useState(false);
+  const sizeClass = variantsizes[props?.variantsizes || 'medium'];
+  const possition = positionValue[props?.positionValue || 'left'];
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    const value = props.value as string;
+    if (e.key === 'Enter' && value.trim() !== '' && props.isInputTag) {
+      props?.setTags?.([...(props?.tags ?? []), value.trim()]);
+      props?.setValue?.('');
+    }
+  };
+  const handleDeleteTask = (index: number) => {
+    const updatedTasks = props?.tags?.filter((_, i) => i !== index) ?? [];
+    props?.setTags?.(updatedTasks);
+  };
   return (
-    <div>
-      <input type={type} value={value} />
+    <div className="relative w-full ">
+      <HStack className="gap-1 mb-1">
+        {props.label && (
+          <>
+            <div className="text-[#0F1824] font-medium">{props.label}</div>
+            {props.isRequired && <p className="text-red text-lg">*</p>}
+          </>
+        )}
+      </HStack>
+
+      <div
+        className={`relative w-full border rounded-md ${props.isError ? 'border-red' : ''} ${isFocused ? 'border-primary' : 'border-gray-200'}`}
+      >
+        <div className="flex flex-wrap items-center px-2">
+          {props?.tags?.map((task, index) => (
+            <div key={index} className="flex items-center bg-primary-light p-1 rounded mr-2">
+              <span className="text-gray-800">{task}</span>
+              <button
+                className="ml-1 flex justify-center items-center hover:text-red-700"
+                onClick={() => handleDeleteTask(index)}
+                aria-label={`Xóa tag ${task}`}
+              >
+                <FontAwesomeIcon className="text-sm px-1 text-primary text-center" icon={faXmark} />
+              </button>
+            </div>
+          ))}
+          {!props?.isNote ? (
+            <input
+              className={`flex-grow min-w-[120px] ${props.prefix ? 'pl-[30px]' : 'pl-[1px]'} ${
+                props.suffix ? 'pr-[30px]' : 'pr-[1px]'
+              } ${props.isDisabled ? 'cursor-not-allowed text-gray-400' : 'cursor-text'} ${sizeClass} outline-none border-0 rounded-md text-[#0F1824] ${possition} mr-2 ${props.className}`}
+              placeholder={props.placeholder}
+              onChange={props.handleOnchange}
+              onKeyPress={handleKeyPress}
+              type={props.type}
+              value={props.value}
+              aria-invalid={props.isError ? 'true' : 'false'}
+              disabled={props.isDisabled}
+              onFocus={() => setIsFocused(true)}
+              onBlur={() => setIsFocused(false)}
+            />
+          ) : (
+            <div
+              contentEditable={!props.isDisabled}
+              tabIndex={0}
+              className={`flex-grow min-w-[120px] ${props.prefix ? 'pl-[30px]' : 'pl-[2px]'} ${
+                props.suffix ? 'pr-[30px]' : 'pr-[2px]'
+              } ${props.isDisabled ? 'cursor-not-allowed text-gray-400' : 'cursor-text'} ${sizeClass} outline-none border-0 rounded-md text-[#0F1824] ${possition} break-words mr-2 ${props.className}`}
+              onInput={props.handleOnchange}
+              onFocus={() => setIsFocused(props?.isDisabled ? false : true)}
+              onBlur={() => setIsFocused(false)}
+              onKeyDown={(e) => props.isDisabled && e.preventDefault()}
+            >
+              {props.value}
+            </div>
+          )}
+        </div>
+        {props.prefix && (
+          <FontAwesomeIcon
+            icon={props.prefix}
+            className="absolute left-[14px] top-[50%] transform -translate-y-1/2 text-gray-400 cursor-pointer"
+          />
+        )}
+        {props.suffix && (
+          <FontAwesomeIcon
+            icon={props.suffix}
+            className="absolute right-[14px] top-[50%] transform -translate-y-1/2 text-gray-400 cursor-pointer"
+          />
+        )}
+        {props.value && !props.suffix && props.isDeleteContent && (
+          <div
+            onClick={() => props.setValue?.('')}
+            className="absolute right-[14px] top-[50%] transform -translate-y-1/2 text-gray-400 cursor-pointer"
+            aria-label="Xóa nội dung"
+          >
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </div>
+        )}
+      </div>
+      {props.isError && <div className="text-red mt-2 ml-1">{props.helperText}</div>}
     </div>
   );
 };
