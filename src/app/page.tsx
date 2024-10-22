f32e779268cc57914f6f99bf0cd5022552791d43
minhaudev <hau95024@donga.edu.vn>
2024-10-22 22:00:03 +0700

    fix: input and css input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,9 @@
 'use client';
 
-import { useState } from 'react';
-
-import Input from '@/components/atoms/Input';
-
 export default function Home() {
-  const [value, setValue] = useState<number>(0);
-  const onChange = (value: any) => {
-    setValue(value);
-    console.log('24343', value);
-  };
-
   return (
     <>
-      <Input onChange={onChange} type="number" value={value} placeholder="nhập và nhấn enter" />
+      <div>hello word</div>
     </>
   );
 }
